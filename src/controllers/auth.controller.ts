c15638413fb9854a6e252dafda56dfe83ac50c8d
Fabrizio <fabriziocaricato1@gmail.com>
2026-06-05 04:00:56 -0300

    feat: complete login controller with JWT generation

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import User from "../models/user.model"
 import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 const register = async (req: Request, res: Response) => {
   try {
@@ -23,4 +24,25 @@ const register = async (req: Request, res: Response) => {
   }
 }
 
-export { register }
+const login = async (req: Request, res: Response) => {
+  try {
+    const {email, password} = req.body
+    const foundUser = await User.findOne({email})
+    if (!foundUser) {
+      return res.status(404).json({ success: false, message: "User not found" })
+    } else {
+      const isMatch = await bcryptjs.compare(password, foundUser.password)
+      if (!isMatch) {
+        return res.status(401).json({ success: false, message: "Invalid email or password" })
+      } else {
+        const payload = { _id: foundUser._id, firstName: foundUser.firstName, lastName: foundUser.lastName, email: foundUser.email }
+        const token = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: "15m" })
+        return res.status(200).json({ success: true, message: "User logged in successfully", data: { user: payload, token } })
+      }
+    }
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "Internal server error", error: error })
+  }
+}
+
+export { register, login }
